4de1e0d1798b56f2d8607283e31d4707831f1094
IsraGlezP <isra.igp@gmail.com>
2020-09-25 23:02:38 -0500

    Se agregaron las rutas hacia las urls correspondientes de cada aplicación

--- crm_internacional_joyas_relojes/settings.py
+++ crm_internacional_joyas_relojes/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clientes',
+    'dashboard',
+    'inventarios',
+    'pedidos',
+    'ventas'
 ]
 
 MIDDLEWARE = [
